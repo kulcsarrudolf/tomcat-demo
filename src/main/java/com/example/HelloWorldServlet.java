c3b6ac461f86ee039e3548a0b85b1b63ede257f5
kulcsarrudolf <kulcsarrudolf@gmail.com>
2024-11-08 12:05:27 +0200

    added path variable

--- src/main/java/com/example/HelloWorldServlet.java
+++ src/main/java/com/example/HelloWorldServlet.java
@@ -1,4 +1,3 @@
-
 package com.example;
 
 import java.io.IOException;
@@ -8,11 +7,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/hello")
+@WebServlet("/hello/*")
 public class HelloWorldServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().println("<h1>Hello, World!</h1>");
+
+        String pathInfo = request.getPathInfo();
+
+        if (pathInfo == null || pathInfo.equals("/")) {
+            response.getWriter().println("<h1>Hello, World!</h1>");
+        } else {
+            String name = pathInfo.substring(1);
+            response.getWriter().println("<h1>Hello, " + name + "!</h1>");
+        }
     }
 }
